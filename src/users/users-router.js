b3daf78946f469d15dcad6c6c0c2263872b1b5fe
thanneman <thanneman1@gmail.com>
2020-03-26 16:30:16 -0700

    updated router

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -129,8 +129,8 @@ usersRouter
         res.json(res.joke)
     })
     .post(requireAuth, jsonParser, (req, res, next) => {
-        const { question, answer } = req.body
-        const newJoke = { question, answer }
+        const { question, answer, rating } = req.body
+        const newJoke = { question, answer, rating }
 
         for (const [key, value] of Object.entries(newJoke))
             if (value == null)
@@ -139,6 +139,7 @@ usersRouter
                 })
 
         newJoke.user_id = req.user.id
+        newJoke.rating = 1
 
         JokesService.insertUserJoke(
             req.app.get('db'),
