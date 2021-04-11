61ae0efb61275ff2743c48a367d3426b68a94368
thanneman <thanneman1@gmail.com>
2021-04-10 20:12:54 -0700

    updated

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const pg = require('pg');
+pg.defaults.ssl = process.env.NODE_ENV === "production";
 
 module.exports = {
     "migrationsDirectory": "migrations",
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 const app = require('./app');
 const { PORT, DATABASE_URL } = require('./config');
+const pg = require('pg');
+pg.defaults.ssl = process.env.NODE_ENV === "production";
 
 const db = knex({
   client: 'pg',
